6b082243d0a871bbbb90cb6a7fec6bb2211f559c
刘杰 <1774251466@qq.com>
2021-09-17 19:44:22 +0800

    给data.txt追加了内容

--- path模块.js
+++ path模块.js
@@ -6,3 +6,7 @@ fs.readFile(path.join(__dirname,'./date.txt'),'utf-8',(err,data)=>{
     if(err)  return console.log(err.message);
     console.log(data);
 })
+fs.appendFile(path.join(__dirname,'./date.txt'),'hello,nihao',err=>{
+    if(err) return console.log(err.message);
+    console.log('追加内容成功');
+})
